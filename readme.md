8bea0211dd22e0b2eb0c5222d15ec8deb8e7738c
Ralf Kraemer <hello@ralfkraemer.eu>
2022-03-18 10:55:25 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Blender World Builder
+
+### Procedural environment generation with Python
+
+##### Intended for efficient design and development in gaming and VR
+
+© Ralf Kraemer 2022
---
+++ run.py
@@ -0,0 +1,14 @@
+import bpy
+import json
+
+# Set up work environment
+
+# Generate terrain
+
+# Generate water, dirt and natural props
+
+# Generate structures
+
+# Beautify
+
+# Export to GLB
